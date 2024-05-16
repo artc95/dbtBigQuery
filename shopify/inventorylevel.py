b1bbc784d46ac611fb27844f63ce8c5c2b67bb0e
artc95 <arthur@dataroots.io>
2024-05-16 13:07:47 +0200

    consolidate get_endpoints_csvs for locations, inventory_levels and products

--- shopify/inventorylevel.py
+++ shopify/inventorylevel.py
@@ -2,51 +2,31 @@ import pandas as pd
 import requests
 
 
-# def get_shopify_api(shopify_access_token, shopify_url)
-def get_Locations(shopify_access_token):
-    # get list of Locations details, via API GET request
-    url = 'https://0588b9.myshopify.com/admin/api/2024-04/locations.json'
-    headers = {'X-Shopify-Access-Token': shopify_access_token, 'content-type': 'application/json'}
+def request_shopifyAPI(shopify_domain_id: str, shopify_access_token: str,
+                       endpoint: str, parameters: str):
+    """
+    request Shopify API, extract list of dicts from response!
+    - endpoint e.g. "locations"
+    - r_json e.g. {
+        "locations": [{"name": "50 Rideau Street"}, {"name": "Apple Api Shipwire"}, {"name": "Apple Cupertino"}]
+        }
+    - r_list e.g. [{"name": "50 Rideau Street"}, {"name": "Apple Api Shipwire"}, {"name": "Apple Cupertino"}]
+    """
+
+    url = f"https://{shopify_domain_id}.myshopify.com/admin/api/2024-04/{endpoint}.json{parameters}"
+    headers = {"X-Shopify-Access-Token": shopify_access_token, "content-type": 'application/json'}
     r = requests.get(url, headers=headers)
-    
-    locations_json = r.json()
-    locations_list = locations_json['locations']
+    r_json = r.json() # dict with endpoint as key, list of dicts as value
+    r_list = r_json[endpoint]  # extract list of dicts from json
 
-    return locations_list
-
-def get_InventoryLevels(locations_list, shopify_access_token):
-    # get list of InventoryLevels, via API GET request
-    url = 'https://0588b9.myshopify.com/admin/api/2024-04/inventory_levels.json?location_ids=95427658037'
-    headers = {'X-Shopify-Access-Token': shopify_access_token, 'content-type': 'application/json'}
-    r = requests.get(url, headers=headers)
-
-    inventory_levels = r.json()
-    inventory_levels_list = inventory_levels['inventory_levels']
-    print(f'arthur inventorylevels: {inventory_levels_list}')
-
-    return inventory_levels_list
-
-def get_Products(shopify_access_token):
-    # get list of InventoryLevels, via API GET request
-    url = 'https://0588b9.myshopify.com/admin/api/2024-04/products.json'
-    headers = {'X-Shopify-Access-Token': shopify_access_token, 'content-type': 'application/json'}
-    r = requests.get(url, headers=headers)
-
-    products = r.json()
-    products_list = products['products']
-    print(f'arthur products: {products_list}')
-    return products_list
+    return r_list
 
 def to_csv(list_of_dicts: list, output_name: str,
            sort_by: list, sort_asc: bool):
     """
     converts a list of dicts, to a .csv file!
-    list_of_dicts: e.g. [
-        {'inventory_item_id': 49661765812533, 'location_id': 95427658037, 'available': 1}, 
-        {'inventory_item_id': 49661765550389, 'location_id': 95427658037, 'available': 1}, 
-        {'inventory_item_id': 49592716230965, 'location_id': 95427658037, 'available': 1}
-        ]
-    sort_by: e.g. ['inventory_item_id']
+    - list_of_dicts: e.g. [{"name": "50 Rideau Street"}, {"name": "Apple Api Shipwire"}, {"name": "Apple Cupertino"}]
+    - sort_by: e.g. ['inventory_item_id']
     """
     df = pd.DataFrame(list_of_dicts)
     df = df.sort_values(by=sort_by, ascending=sort_asc)
@@ -54,11 +34,30 @@ def to_csv(list_of_dicts: list, output_name: str,
     df.to_csv(f'{output_name}.csv', index=False)
     print(f'{output_name}.csv successfully created!')
 
+def get_endpoints_csvs(shopify_domain_id: str, shopify_access_token: str,
+                       endpoints_dict: dict):
+    """
+    request Shopify API and convert to .csv, using dict of endpoints and specifications
+    - endpoints_dict e.g. {"locations": {"parameters": "", "sort_by": "id", "sort_asc": True}, 
+                           "inventory_levels": {"parameters": "a", "sort_by": "inventory_item_id", "sort_asc": True}}
+    """
 
+    for endpoint in endpoints_dict.keys():
+        # unpack values of dict for each endpoint
+        parameters, sort_by, sort_asc = endpoints_dict[endpoint].values()
+        
+        list_of_dicts = request_shopifyAPI(shopify_domain_id, shopify_access_token, 
+                                           endpoint, parameters)
+        to_csv(list_of_dicts, endpoint, sort_by, sort_asc)
+
+shopify_domain_id = '0588b9'
 shopify_access_token = <token>
+endpoints_dict = {
+    "locations": {"parameters": "", "sort_by": "id", "sort_asc": True}, 
+    # TODO: SEQUENTIAL DEPENDENCY e.g. inventory_levels should take in location_ids from locations
+    "inventory_levels": {"parameters": "?location_ids=95427658037", "sort_by": "inventory_item_id", "sort_asc": True},
+    "products": {"parameters": "", "sort_by": "title", "sort_asc": True}
+    }
 
-locations_list = get_Locations(shopify_access_token)
-to_csv(locations_list, 'locations', ['id'], sort_asc=False)
-
-inventory_levels_list = get_InventoryLevels(locations_list, shopify_access_token)
-products_list = get_Products(shopify_access_token)
+# create .csvs for multiple endpoints
+get_endpoints_csvs(shopify_domain_id, shopify_access_token, endpoints_dict)
