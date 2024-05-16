8ad71a02c4679c794f98bc78a264730c96a445cb
artc95 <arthur@dataroots.io>
2024-05-16 10:16:08 +0200

    get Products

--- shopify/inventorylevel.py
+++ shopify/inventorylevel.py
@@ -1,7 +1,8 @@
 import requests
 
 
-def get_locations(shopify_access_token):
+# def get_shopify_api(shopify_access_token, shopify_url)
+def get_Locations(shopify_access_token):
     # get list of Locations details, via API GET request
     url = 'https://0588b9.myshopify.com/admin/api/2024-04/locations.json'
     headers = {'X-Shopify-Access-Token': shopify_access_token, 'content-type': 'application/json'}
@@ -10,27 +11,33 @@ def get_locations(shopify_access_token):
     locations_json = r.json()
     locations_list = locations_json['locations']
 
-    # extract key Locations details, store as dict of dicts (key is Location id, value is dict of Locations details)
-    locations_dict = {}
-    for location in locations_list:
-        location_id = location['id']
-        locations_dict[location_id] = { key: location[key] for key in ['active', 'city', 'country', 'country_code', 'created_at', 
-                                                    'id', 'name', 'updated_at']}
-    # print(locations_dict)
-    return locations_dict
+    return locations_list
 
-def get_inventoryLevel(locations_dict, shopify_access_token):
+def get_InventoryLevels(locations_list, shopify_access_token):
     # get list of InventoryLevels, via API GET request
     url = 'https://0588b9.myshopify.com/admin/api/2024-04/inventory_levels.json?location_ids=95427658037'
     headers = {'X-Shopify-Access-Token': shopify_access_token, 'content-type': 'application/json'}
     r = requests.get(url, headers=headers)
 
     inventory_levels = r.json()
-    inventory_levels = inventory_levels['inventory_levels']
-    print(inventory_levels)
+    inventory_levels_list = inventory_levels['inventory_levels']
+    print(f'arthur inventorylevels: {inventory_levels_list}')
+
+    return inventory_levels_list
+
+def get_Products(shopify_access_token):
+    # get list of InventoryLevels, via API GET request
+    url = 'https://0588b9.myshopify.com/admin/api/2024-04/products.json'
+    headers = {'X-Shopify-Access-Token': shopify_access_token, 'content-type': 'application/json'}
+    r = requests.get(url, headers=headers)
+
+    products = r.json()
+    products_list = products['products']
+    print(f'arthur products: {products_list}')
+    return products_list
 
 
-
-shopify_access_token = <shopify_access_token>
-locations_dict = get_locations(shopify_access_token)
-inventory_levels = get_inventoryLevel(locations_dict, shopify_access_token)
+shopify_access_token = <token>
+locations_list = get_Locations(shopify_access_token)
+inventory_levels_list = get_InventoryLevels(locations_list, shopify_access_token)
+products_list = get_Products(shopify_access_token)
